48bf3db2d780dbdb8773a5b849358714a2190613
Camden Weaver <andrea.caudle@killeenisd.org>
2018-10-15 13:51:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
+from urllib.request import urlopen
 commands = ["help","likebot","dislikebot","pflikebot","pfdislikebot"]
 def parse(cmd):
   if cmd not in commands:
     print("Invalid command. Type help for a list of commands")
+  elif cmd == "help":
+    print("likebot - likebots a comment\ndislikebot - dislikebots a comment\npflikebot - likebots a profile post\npfdislikebot - dislikebots a profile post")
   else:
-    
+    print("Enter username")
+    uname = input(">>>")
+    urlopen("http://sheltered-castle-48436.herokuapp.com/{}/username/{}".format(cmd,uname))
+    print("Success")
 def main():
   print("Camden's 2.11 Likebot")
   print("Type help for a list of commands")
