8f70eb2b2e677273d70a0a313a2ed28012709716
Camden Weaver <andrea.caudle@killeenisd.org>
2018-10-15 13:52:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from urllib.request import urlopen
-commands = ["help","likebot","dislikebot","pflikebot","pfdislikebot"]
+commands = ["help","likebot","dislikebot","pflikebot","pfdislikebot","exit"]
 def parse(cmd):
   if cmd not in commands:
     print("Invalid command. Type help for a list of commands")
   elif cmd == "help":
     print("likebot - likebots a comment\ndislikebot - dislikebots a comment\npflikebot - likebots a profile post\npfdislikebot - dislikebots a profile post")
+  elif cmd == "exit:
+    exit()
   else:
     print("Enter username")
     uname = input(">>>")
@@ -15,3 +17,5 @@ def main():
   print("Type help for a list of commands")
   result = input(">>>")
   parse(result)
+while True:
+  main()
